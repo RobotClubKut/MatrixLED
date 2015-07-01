ee14b14725849471280826aadfa330b09b94caa4
＼ﾋｯｶﾘｰﾝ／ <s.nlf.fh@gmail.com>
2015-07-01 22:44:45 +0900

    add test0

---
+++ TEST0/TEST0.cydsn/main.c
@@ -0,0 +1,30 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+	uint8 i;
+	
+	CyGlobalIntEnable;
+	
+	//USBUART_Start(0, USBUART_5V_OPERATION);
+	//while (USBUART_GetConfiguration() == 0);
+    //USBUART_CDC_Init();
+	
+	for (;;)
+    {
+		
+    }
+}
+
+/* [] END OF FILE */
