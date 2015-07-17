464b9730328a74403dd6f15d3ed18298bbf7c85a
kkumt93 <naniwa.k.m0903@gmail.com>
2015-07-17 17:32:46 +0900

    update

--- Matrix_test/Font_test.c
+++ Matrix_test/Font_test.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include "DotMatrix.h"
-#include "jiskan_lib.h"
+#include "jiskan.h"
+
+
 
 int main(){
 
 	dotMatrix dotMat;
 	int x, y;
-	unsigned int jis_num = 319;
+	unsigned int jis_num = 1200;
 	dotMatrix_clear(&dotMat);
 
 	for(y=0;y<15;y++){
 		for(x=0;x<15;x++){
-			if(jis_font[16*jis_num+y] & (1 << x)){
+			if(jis_font[font_line*jis_num+y] & (1 << x)){
 				dotMat.array[15-x][y][0] = 1;
 			}
 			else{
@@ -19,6 +21,8 @@ int main(){
 			}
  		}
 	}
+
+
 
 	dotMatrix_print(&dotMat);
 }
